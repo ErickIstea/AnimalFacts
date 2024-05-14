e163a541fc753f2e2b76bd25cb3b366f0fcb50a8
ErickIstea <erick.silva@istea.com.ar>
2024-05-14 20:14:50 -0300

    Conecto los estados y las intenciones del MVI

--- app/src/main/java/com/istea/animalfacts/main/MainPage.kt
+++ app/src/main/java/com/istea/animalfacts/main/MainPage.kt
@@ -1,9 +1,13 @@
 package com.istea.animalfacts.main
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.istea.animalfacts.ui.theme.AnimalFactsTheme
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -18,9 +22,42 @@ fun MainPage(modifier: Modifier = Modifier) {
 
 @Composable
 fun MainView(modifier: Modifier = Modifier, estado: MainEstado, ejecutar: (MainIntencion)->Unit){
+
+    Column(modifier = modifier.padding(20.dp)) {
+
+        when(estado){
+            is MainEstado.Correcto -> MainViewCorrecto()
+            is MainEstado.Error -> MainViewError()
+            is MainEstado.Cargando -> MainViewCargando()
+        }
+
+        Button(onClick = { ejecutar(MainIntencion.Refrescar) }) {
+            Text(text = "Refrescar")
+        }
+        Button(onClick = { ejecutar(MainIntencion.RomperTodo) }) {
+            Text(text = "Romper todo")
+        }
+    }
+}
+
+@Composable
+fun MainViewCorrecto(){
     Text(
-        modifier = modifier,
-        text = "Hello!"
+        text = "Correcto"
+    )
+}
+
+@Composable
+fun MainViewCargando(){
+    Text(
+        text = "Cargando"
+    )
+}
+
+@Composable
+fun MainViewError(){
+    Text(
+        text = "Error"
     )
 }
 
--- app/src/main/java/com/istea/animalfacts/main/MainViewModel.kt
+++ app/src/main/java/com/istea/animalfacts/main/MainViewModel.kt
@@ -7,10 +7,21 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
 
-    var estadoDeUI by mutableStateOf<MainEstado>(MainEstado.Cargando)
+    var estadoDeUI by mutableStateOf<MainEstado>(MainEstado.Correcto)
 
     fun ejecutar(intencion: MainIntencion){
+        when(intencion){
+            MainIntencion.Refrescar -> refrescar()
+            MainIntencion.RomperTodo -> romperTodo()
+        }
+    }
 
+    private fun refrescar(){
+        estadoDeUI = MainEstado.Cargando
+    }
+
+    private fun romperTodo(){
+        estadoDeUI = MainEstado.Error
     }
 }
 
